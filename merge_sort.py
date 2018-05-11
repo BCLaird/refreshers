7b9f726bf53875bb68fbf73451ee28b5383480a0
Bryan <bryan@bclaird.com>
2018-05-11 18:33:38 -0400

    Completed writing merge_sort function.

--- merge_sort.py
+++ merge_sort.py
@@ -1,14 +1,45 @@
-import os
 import sys
 import unittest
 
 
+def merge(nums1, nums2):
+    """
+    :param nums1: Sorted list of numbers.
+    :param nums2: Sorted list of numbers.
+    :return: Combined sorted list of numbers.
+    """
+
+    merged = list()
+
+    while len(nums1) != 0 and len(nums2) != 0:
+        if nums1[0] < nums2[0]:
+            merged.append(nums1.pop(0))
+        else:
+            merged.append(nums2.pop(0))
+
+    while len(nums1) != 0:
+        merged.append(nums1.pop(0))
+
+    while len(nums2) != 0:
+        merged.append(nums2.pop(0))
+
+    return merged
+
+
 def merge_sort(nums):
+    """
+    :param nums: List of numbers to sort.
+    :return: Sorted list of  numbers.
+    """
 
     if len(nums) != 1:
-        pass
-
-    return nums
+        nums1 = merge_sort(nums[:(len(nums) / 2)])
+        nums2 = merge_sort(nums[(len(nums) / 2):])
+        sorted_nums = merge(nums1, nums2)
+        return sorted_nums
+    else:
+        # Nothing to do for a list of length 1.
+        return nums
 
 
 class TestInsertionSort(unittest.TestCase):
